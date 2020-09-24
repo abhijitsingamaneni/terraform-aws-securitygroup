f8772b1a522c3107bf127d026bb0229449e623e7
abhijit <abhijitcs1417@gmail.com>
2020-09-24 18:26:54 +0530

    adding security groups

---
+++ main.tf
@@ -0,0 +1,43 @@
+
+
+# ALB security group 
+resource "aws_security_group" "lb" {
+  name        = "${var.env}-load-balancer-security-group-${var.vpc_name}"
+  description = "controls access to the ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Traffic to the ECS cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.env}-ecs-tasks-security-group-${var.vpc_name}"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol        = "-1"
+    from_port       = 0
+    to_port         = 0
+    security_groups = [aws_security_group.lb.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+    description = "vpc under which we need to creart the VPC"
+}
+variable "env" {
+    description = "what is the env"
+}
+variable "vpc_name" {
+    description = "what is the name of the VPC"
+}
